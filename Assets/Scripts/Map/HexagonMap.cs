426adc63fe2fc65f55c04c8953fe51445b9f5873
TwentyFive <TwentyFiveSoftware@gmail.com>
2022-05-13 15:29:46 +0200

    only generate default hexagons around paths

--- Assets/Scripts/Map/HexagonMap.cs
+++ Assets/Scripts/Map/HexagonMap.cs
@@ -25,6 +25,7 @@ public class HexagonMap : MonoBehaviour {
 
     private void GenerateMap(List<Building> buildings) {
         List<Vector2Int> generatedHexagonPositions = new List<Vector2Int>();
+        List<Vector2Int> mapOutline = new List<Vector2Int>();
 
         List<Path> paths = new List<Path>();
 
@@ -32,6 +33,7 @@ public class HexagonMap : MonoBehaviour {
             float height = 3.0f + GetHeightAt(building.location);
             GenerateHexagonAt(building.location, HexagonType.BUILDING, height);
             generatedHexagonPositions.Add(building.location);
+            mapOutline.Add(building.location);
 
             foreach (Path pathToDestination in building.destinations.Values) {
                 if (!paths.Contains(pathToDestination)) {
@@ -41,23 +43,43 @@ public class HexagonMap : MonoBehaviour {
         }
 
         foreach (Path path in paths) {
-            foreach (Vector2Int pathHexagon in path.hexagons) {
+            for (int i = 0; i < path.hexagons.Count; i++) {
+                Vector2Int pathHexagon = path.hexagons[i];
                 float height = 1.0f + GetHeightAt(pathHexagon);
                 GenerateHexagonAt(pathHexagon, HexagonType.PATH, height);
                 generatedHexagonPositions.Add(pathHexagon);
+
+                if (i % 5 == 0) {
+                    mapOutline.Add(pathHexagon);
+                }
             }
         }
 
-        // for (int x = -buildingsBaseDistance / 2; x < buildingsPerAxis * buildingsBaseDistance; x++) {
-        //     for (int z = -buildingsBaseDistance / 2; z < buildingsPerAxis * buildingsBaseDistance; z++) {
-        //         Vector2Int position = new Vector2Int(x - z / 2, z);
-        //
-        //         if (!generatedHexagonPositions.Contains(position)) {
-        //             float height = 0.5f + GetHeightAt(position);
-        //             GenerateHexagonAt(position, HexagonType.DEFAULT, height);
-        //         }
-        //     }
-        // }
+        foreach (Vector2Int outlinePoint in mapOutline) {
+            Vector3 pointWorldPosition = Hexagon.CalculateHexagonWorldPosition(outlinePoint);
+
+            for (int x = -buildingsBaseDistance; x < buildingsBaseDistance; x++) {
+                for (int z = -buildingsBaseDistance; z < buildingsBaseDistance; z++) {
+                    Vector2Int position = outlinePoint + new Vector2Int(x - z / 2, z);
+
+                    if (generatedHexagonPositions.Contains(position)) {
+                        continue;
+                    }
+
+                    float distance = (Hexagon.CalculateHexagonWorldPosition(position) - pointWorldPosition).magnitude;
+
+                    // if (distance > buildingsBaseDistance * 1.05) {
+                    if (distance > buildingsBaseDistance * 1.1) {
+                        continue;
+                    }
+
+                    generatedHexagonPositions.Add(position);
+
+                    float height = 0.5f + GetHeightAt(position);
+                    GenerateHexagonAt(position, HexagonType.DEFAULT, height);
+                }
+            }
+        }
     }
 
     private float GetHeightAt(Vector2Int position) {
